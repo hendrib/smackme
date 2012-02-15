25876bd2226146bc9830616a5c3c2922d195ae76
hendrib <hendrib@cdev03.miranetworks.net>
2012-02-15 16:09:09 +0200

    mochiwebifying smackme

---
+++ apps/smackme/src/smackme_web.erl
@@ -0,0 +1,56 @@
+-module(smackme_web).
+
+-export([start/1, stop/0, loop/2]).
+
+%% External API
+
+start(Options) ->
+    {DocRoot, Options1} = get_option(docroot, Options),
+    Loop = fun (Req) ->
+                   ?MODULE:loop(Req, DocRoot)
+           end,
+    mochiweb_http:start([{name, ?MODULE}, {loop, Loop} | Options1]).
+
+stop() ->
+    mochiweb_http:stop(?MODULE).
+
+loop(Req, DocRoot) ->
+    "/" ++ Path = Req:get(path),
+    try
+        case Req:get(method) of
+            Method when Method =:= 'GET'; Method =:= 'HEAD' ->
+                case Path of
+				"smackme" ->
+					Smack = smackme_smack_server:smackme(),
+					
+					Req:respond({200, [{"Content-Type", "text/plain"}], Smack ++ "\n"});
+
+                _ ->
+					Req:respond({200, [{"Content-Type", "text/plain"}], "No smack\n"})
+                        %Req:serve_file(Path, DocRoot)
+                end;
+            'POST' ->
+                case Path of
+                    _ ->
+                        Req:not_found()
+                end;
+            _ ->
+                Req:respond({501, [], []})
+        end
+    catch
+        Type:What ->
+            Report = ["web request failed",
+                      {path, Path},
+                      {type, Type}, {what, What},
+                      {trace, erlang:get_stacktrace()}],
+            error_logger:error_report(Report),
+            %% NOTE: mustache templates need \ because they are not awesome.
+            Req:respond({500, [{"Content-Type", "text/plain"}],
+                         "request failed, sorry\n"})
+    end.
+
+%% Internal API
+
+get_option(Option, Options) ->
+    {proplists:get_value(Option, Options), proplists:delete(Option, Options)}.
+
